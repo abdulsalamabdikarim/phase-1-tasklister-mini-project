715a7507e3c2e10da4e39741f6a4cf3090f31f67
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-09-30 23:55:07 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener(`DOMContentLoaded`, () =>{
+  const form = document.querySelector(`form`);
+  form.addEventListener(`submit`, (e) =>{
+    e.preventDefault();
+    const input = document.querySelector("#new-task-description");
+    addToDo(input.value);
+    form.reset();
+  })
+})
+function addToDo(todo){
+  //console.log(todo);
+  let li = document.createElement(`li`);
+  li.textContent = `${todo}  `;
+  let btn = document.createElement(`button`);
+  btn.textContent = `x`;
+  btn.addEventListener(`click`, deleteToDo)
+  let tasks = document.getElementById(`tasks`);
+  tasks.appendChild(li);
+  li.appendChild(btn);
+}
+
+function deleteToDo(e){
+  e.target.parentNode.remove();
+}
+
+
